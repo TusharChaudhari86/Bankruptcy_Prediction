b856da9059908204c262ef1c61dcac0ad85ad4b4
Tushar Chaudhari <43539598+TusharChaudhari86@users.noreply.github.com>
2020-06-13 15:35:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,6 @@ def trainRouteClient():
     return Response("Training successfull!!")
 
 
-port = int(os.getenv("PORT",5000))
+)
 if __name__ == "__main__":
-    app.run(port=port,debug=True)
+    app.run(debug=True)
